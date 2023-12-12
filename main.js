18bb4791f5f60242a9e919a5aee7f2e99042fe5c
DEPSTRCZ <jiri.edelmann@gmail.com>
2023-12-12 10:29:17 +0100

    whatever

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 console.log("Hello World\nThe time is:",new Date().toLocaleString());
-
+/*
 const n = 5
 let out = ""
 for (i = 1; i <= 10; i++) {
@@ -10,3 +10,15 @@ for (i = 1; i <= 10; i++) {
     if (i < 10 ) out+="-"
 }
 console.log(out.slice(0,out.length))
+*/
+
+const lines = 5
+const size = 13
+for (i = 0  ; i < lines; i++) {
+    let elem = i.toString().padStart(size,"0")+"-"
+    let spacing = "-".repeat((lines-i-1)*(Math.round(size/2)))
+    let elements = elem.repeat(i+1)
+    console.log(spacing+elements)
+}
+
+//pascaluv trojuhelnik
